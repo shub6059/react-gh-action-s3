394500a9661e964d832f3968ff229bb24f2e01db
Shubham Patil <68894316+shub6059@users.noreply.github.com>
2024-01-08 11:17:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hiiiii Its Shubham Patil!
+          Hiiiii Its Shubham !
         </p>
         <a
           className="App-link"
